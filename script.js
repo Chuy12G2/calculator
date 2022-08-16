03a3c53856c6bab854d0636ce42367dcad122c27
Jesus <chuygs12@gmail.com>
2022-08-16 18:32:22 -0500

    add UI

--- script.js
+++ script.js
@@ -14,6 +14,25 @@ const divide = (num1, num2) => {
     return num1 / num2;
 }
 
+const operate = (operator, num1, num2) => {
+    if (operator === "+"){
+        return add(num1,num2);
+    }
+
+    else if (operator === "-"){
+        return subtract(num1,num2);
+    }
+
+    else if (operator === "*"){
+        return multiply(num1,num2);
+    }
+    
+    else if (operator === "/"){
+        return divide(num1,num2);
+    }
+
+}
+console.log(operate("-", 12, 21));
 
 
-console.log(divide(32, 98));
+//console.log(divide(32, 98));
